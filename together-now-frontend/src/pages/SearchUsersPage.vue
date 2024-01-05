05a1782a24c98ca7c5e9f7ca3f12b4f934b9e3cf
CCCshengjiang <123shengjiang@gmail.com>
2024-01-05 21:26:16 +0800

    🌐 locale(搜索用户): 完善向后端请求数据

--- together-now-frontend/src/pages/SearchUsersPage.vue
+++ together-now-frontend/src/pages/SearchUsersPage.vue
@@ -1,47 +1,61 @@
 <script setup lang="ts">
 import {useRoute} from "vue-router";
-import {ref} from "vue";
+import {onMounted, ref} from "vue";
+import myAxios from "../plugs/MyAxios.js"
+import qs from 'qs';
 
 const route = useRoute();
 
-const { tag } = route.query;
+const {tags} = route.query;
 
+const userList = ref([])
 
+onMounted(async() => {
+// Optionally the request above could also be done as
+  const searchUserList = await myAxios.get('/user/search/tags', {
+    params: {
+      tagNameList: tags,
+    },
+    paramsSerializer: params => {
+      return qs.stringify(params, { indices: false })
+    }
+  })
+      .then(function (response) {
+        console.log('/user/search/tags succeed', response);
+        return response.data?.data;
+      })
+      .catch(function (error) {
+        console.log('/user/search/tags error', error);
+      })
+  if (searchUserList) {
+    searchUserList.forEach(user => {
+      if (user.tags) {
+        user.tags = JSON.parse(user.tags);
+      }
+    })
+    userList.value = searchUserList;
+  }
+})
 
-const mockUser = {
-  id: 1,
-  username: 'wen',
-  avatarUrl: 'https://img2.baidu.com/it/u=4260210395,2753506949&fm=253&app=138&size=w931&n=0&f=JPEG&fmt=auto?sec=1704474000&t=bc650d45794e38cbcc9b7ee86bcb6de4',
-  userProfile: '大四学生，正在做项目together-now，在十五号回家，买的火车票 + 高铁票',
-  gender: '男',
-  userAccount: '666',
-  phone: '12345678911',
-  email: '123@qq.com',
-  idCode: '1',
-  tag: ['java', 'c++'],
-  createTime: new Date(),
-}
-
-const userList = ref([mockUser])
 </script>
 
 <template>
   <van-card
       v-for="user in userList"
       :desc="user.userProfile"
-      :title="`${user.username} (${user.idCode})`"
+      :title="`${user.username} (编号：${user.idCode})`"
       :thumb="user.avatarUrl"
   >
     <template #tags>
       <van-tag plain type="primary" v-for="tag in user.tag" style="margin-right: 8px; margin-top: 8px">
-        {{tag}}
+        {{ tag }}
       </van-tag>
     </template>
     <template #footer>
       <van-button size="mini">联系我</van-button>
     </template>
   </van-card>
-
+  <van-empty image="search" v-if="!userList || userList.length === 0" description="暂无符合要求用户" />
 </template>
 
 <style scoped>
