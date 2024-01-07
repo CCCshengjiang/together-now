c764fdf7fa133dec21db16648ed7d41214cddffa
CCCshengjiang <123shengjiang@gmail.com>
2024-01-08 01:43:49 +0800

    💥 feat(定时任务): 实现分布式锁

---
+++ together-now-backend/src/main/java/com/wen/togethernow/config/RedissonConfig.java
@@ -0,0 +1,41 @@
+package com.wen.togethernow.config;
+
+import lombok.Data;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Redisson的配置
+ *
+ * @author wen
+ */
+@Configuration
+@ConfigurationProperties(value = "spring.data.redis")
+@Data
+public class RedissonConfig {
+    /**
+     * redis地址
+     */
+    private String host;
+
+    /**
+     * redis端口号
+     */
+    private String port;
+
+    @Bean
+    public RedissonClient redissonClient() {
+        // 1. 创建配置对象
+        Config config = new Config();
+        String redisAddress = String.format("redis://%s:%s", host, port);
+        config.useSingleServer().setAddress(redisAddress).setDatabase(1);
+        // 2. 创建Redisson实例
+        // Sync and Async API
+        return Redisson.create(config);
+    }
+
+}
--- together-now-backend/src/main/java/com/wen/togethernow/job/PreCacheJob.java
+++ together-now-backend/src/main/java/com/wen/togethernow/job/PreCacheJob.java
@@ -2,16 +2,16 @@ package com.wen.togethernow.job;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.wen.togethernow.mapper.UserMapper;
 import com.wen.togethernow.model.domain.User;
 import com.wen.togethernow.service.UserService;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -28,6 +28,9 @@ public class PreCacheJob {
     private UserService userService;
 
     @Resource
+    private RedissonClient redissonClient;
+
+    @Resource
     private RedisTemplate<String, Object> redisTemplate;
 
     // TODO 动态设置重点用户
@@ -36,18 +39,31 @@ public class PreCacheJob {
     /**
      * 每天凌晨两点执行，预热推荐用户
      */
-    @Scheduled(cron = "0 0 2 * * *")
+    @Scheduled(cron = "0 32 1 * * *")
     public void doPreCacheRecommend() {
-        for (Long userId : mainUsersId) {
-            QueryWrapper<User> queryWrapper = new QueryWrapper<>();
-            Page<User> userPage = userService.page(new Page<>(1, 20), queryWrapper);
-            String redisKey = String.format("togethernow:user:recommend:%s", userId);
+        RLock lock = redissonClient.getLock("together:precachejob:docache:lock");
+        try {
+            // 只抢一次抢不到就放弃，锁的过期时间：1min
+            if (lock.tryLock(0, 60000, TimeUnit.MILLISECONDS)) {
+                for (Long userId : mainUsersId) {
+                    QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+                    Page<User> userPage = userService.page(new Page<>(1, 20), queryWrapper);
+                    String redisKey = String.format("togethernow:user:recommend:%s", userId);
 
-            // 将查询到的数据写到缓存，设置过期时间为：12h
-            try {
-                redisTemplate.opsForValue().set(redisKey, userPage, 12, TimeUnit.HOURS);
-            } catch (Exception e) {
-                log.error("Redis set key error", e);
+                    // 将查询到的数据写到缓存，设置过期时间为：2h
+                    try {
+                        redisTemplate.opsForValue().set(redisKey, userPage, 2, TimeUnit.HOURS);
+                    } catch (Exception e) {
+                        log.error("Redis set key error", e);
+                    }
+                }
+            }
+        } catch (InterruptedException e) {
+            log.error("doPreCacheRecommend error", e);
+        } finally {
+            // 只能释放自己加的锁
+            if (lock.isHeldByCurrentThread()) {
+                lock.unlock();
             }
         }
     }
