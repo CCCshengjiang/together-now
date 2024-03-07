5f5322312a7e5eea8dc1c168450620c805eb8a70
CCCshengjiang <123shengjiang@gmail.com>
2024-03-08 00:31:48 +0800

    💥 feat(组队): 美化修改个人信息的排版

--- together-now-frontend/src/pages/users/UserUpdatePage.vue
+++ together-now-frontend/src/pages/users/UserUpdatePage.vue
@@ -40,11 +40,11 @@ function padZero(num: number) {
 <template>
   <div v-if="user">
     <van-cell title="编号" :value="user.idCode" />
+    <van-cell title="账号" :value="user.userAccount" />
     <van-cell title="头像" is-link to="/user/edit" @click="toEdit('avatarUrl', '头像', user.avatarUrl)">
       <img style="height: 47px" :src="user.avatarUrl" alt=""/>
     </van-cell>
     <van-cell title="用户名" is-link to="/user/edit" :value="user.username" @click="toEdit('username', '用户名', user.username)" />
-    <van-cell title="账号" is-link to="/user/edit" :value="user.userAccount" @click="toEdit('userAccount', '账号', user.userAccount)" />
     <van-cell title="性别" is-link to="/user/edit" :value="user.gender === 1 ? '男' : '女'" @click="toEdit('gender', '性别', user.gender === 1 ? '男' : '女')" />
     <van-cell title="电话" is-link to="/user/edit" :value="user.phone" @click="toEdit('phone', '电话', user.phone)" />
     <van-cell title="邮箱" is-link to="/user/edit" :value="user.email" @click="toEdit('email', '邮箱', user.email)" />
